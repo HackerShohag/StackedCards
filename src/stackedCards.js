1d8cb739a1452b1bdb438a91363c10c85f69313f
HackerShohag <HackerShohag@outlook.com>
2023-07-16 01:19:59 +0600

    added swipe out animation (one side)

--- src/stackedCards.js
+++ src/stackedCards.js
@@ -4,10 +4,10 @@
 * Author: Juned Chhipa
 */
 
-(function(){
-    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }
+(function () {
+    bind = function (fn, me) { return function () { return fn.apply(me, arguments); }; }
 
-    this.stackedCards = (function() {
+    this.stackedCards = (function () {
         stackedCards.prototype.defaults = {
             layout: 'slide',                     // slide, fanOut
             onClick: undefined,                 // onclick event provided
@@ -44,123 +44,120 @@
             this.parent = els[0].parentNode;
 
             // Sets height of cards adjusted to the height of the content of the tallest card
-            var getItemHeight = me.getHeight().max; 
+            var getItemHeight = me.getHeight().max;
             els.forEach(item => item.style.height = parseInt(getItemHeight) + "px");
-            
+
             // to get the active element's position, we will have to know if elements are in even/odd count
-            var lenAdjust = (els.length%2==0 ? -2 : -1)
-            
+            var lenAdjust = (els.length % 2 == 0 ? -2 : -1)
+
             // oneHalf if the centerPoint - things go left and right from here
-            var oneHalf = (els.length+lenAdjust)/2;
+            var oneHalf = (els.length + lenAdjust) / 2;
 
-            var activeTransform = "translate("+ -50 +"%, 0%)  scale(1)";
+            var activeTransform = "translate(" + -50 + "%, 0%)  scale(1)";
 
-            this.detectSwipe(); 
+            this.detectSwipe();
 
 
-            Array.prototype.forEach.call(els, function(el) {
+            Array.prototype.forEach.call(els, function (el, index) {
 
                 el.style.transformOrigin = me.config.transformOrigin;
 
-                el.addEventListener("click", function() {
+                el.addEventListener("click", function () {
 
                     var clickedEl = el;
                     var nextCnt = 0;
                     var prevCnt = 0;
 
-                    do  {
+                    do {
                         // While there is a next sibling, loop
                         var next = clickedEl.nextElementSibling;
                         nextCnt = nextCnt + 1;
-                        
-                    } while(clickedEl = clickedEl.nextElementSibling);
-                    
+
+                    } while (clickedEl = clickedEl.nextElementSibling);
+
                     // re-initialize the clickedEl to do the same for prev elements
                     clickedEl = el;
 
                     do {
                         // While there is a prev sibling, loop
                         var prev = clickedEl.previousElementSibling;
-                        prevCnt = prevCnt + 1; 
-                    } while(clickedEl = clickedEl.previousElementSibling);
-                    
+                        prevCnt = prevCnt + 1;
+                    } while (clickedEl = clickedEl.previousElementSibling);
+
                     me.reCalculateTransformsOnClick(nextCnt - 1, prevCnt - 1)
 
-                    me.loopNodeList(els, function(el) {
+                    me.loopNodeList(els, function (el) {
                         el.classList.remove("active");
                     })
 
                     el.classList.add("active");
                     el.classList.add(me.config.layout)
 
-                    
-
-                    el.style.zIndex = els.length*5;
+                    el.style.zIndex = els.length * 5;
                     el.style.transform = activeTransform;
 
                     if (me.config.onClick !== undefined) {
-                         me.config.onClick(el);
+                        me.config.onClick(el);
                     }
-
                 });
             });
 
             els[oneHalf].click();
-              
+
         }
 
-        stackedCards.prototype.getHeight = function() {
+        stackedCards.prototype.getHeight = function () {
 
             var es = this.nodelistToArray(this.els);
 
-            var elHeights = els.map(item => item.scrollHeight).sort((a, b)=>b-a);
+            var elHeights = els.map(item => item.scrollHeight).sort((a, b) => b - a);
             var maxHeight = elHeights[0];
 
             return { heights: elHeights, max: maxHeight };
         }
 
-        stackedCards.prototype.reCalculateTransformsOnClick = function(nextCnt, prevCnt) {
+        stackedCards.prototype.reCalculateTransformsOnClick = function (nextCnt, prevCnt) {
             var me = this;
             var maxHeight = me.getHeight().max;
-            var vertOffsets = me.getHeight().heights.map(item=> Math.round((( 1 - ( item / maxHeight )) * -100) ));
-            
+            var vertOffsets = me.getHeight().heights.map(item => Math.round(((1 - (item / maxHeight)) * -100)));
+
             var z = 10;
 
             var els = this.nodelistToArray(this.els);
 
             els[0].parentNode.style.height = parseInt(me.getHeight().max) + "px";
 
-            var scale = 1, translateX = 0, rotateVal=0, rotate="";
+            var scale = 1, translateX = 0, rotateVal = 0, rotate = "";
             var rotateNegStart = 0// ((75 / els.length) * (oneHalf))*-1;
 
             var transformArr = [];
             var zIndexArr = [];
             var relArr = [];
 
-            var layout = this.config.layout; 
+            var layout = this.config.layout;
 
             var maxCntDivisor = Math.max(prevCnt, nextCnt);
             var prevDivisor = 100 / (maxCntDivisor);
             var nextDivisor = 100 / (maxCntDivisor);
-            
-            if(prevCnt>nextCnt) {
-                scale = 0 + (100 / (prevCnt+1))/100;
+
+            if (prevCnt > nextCnt) {
+                scale = 0 + (100 / (prevCnt + 1)) / 100;
             }
             else {
-                scale = 1 - ((prevCnt) *(1/(nextCnt+1)));
+                scale = 1 - ((prevCnt) * (1 / (nextCnt + 1)));
             }
-                        
-            var rotatePrevStart = ((prevCnt*10 / (prevCnt) * prevCnt))*-1;
-            var rotateNextStart = ((nextCnt*10 / (nextCnt)));
 
-            for(var i=0; i<prevCnt; i++) {
-                switch(layout) {
-                    case "slide":                        
-                        if(i>0) {
-                            scale = scale + (100 / (maxCntDivisor+1))/100;
+            var rotatePrevStart = ((prevCnt * 10 / (prevCnt) * prevCnt)) * -1;
+            var rotateNextStart = ((nextCnt * 10 / (nextCnt)));
+
+            for (var i = 0; i < prevCnt; i++) {
+                switch (layout) {
+                    case "slide":
+                        if (i > 0) {
+                            scale = scale + (100 / (maxCntDivisor + 1)) / 100;
                         }
-                        
-                        translateX = (-50 - ((prevDivisor)*(prevCnt-i)));
+
+                        translateX = (-50 - ((prevDivisor) * (prevCnt - i)));
                         translateY = vertOffsets[i] / 2;
 
                         rotate = "rotate(0deg)";
@@ -168,43 +165,45 @@
                     case "fanOut":
                         rotateVal = rotatePrevStart;
 
-                        if(i>0) {
-                            scale = scale + (100 / (maxCntDivisor+1))/100;
+                        if (i > 0) {
+                            scale = scale + (100 / (maxCntDivisor + 1)) / 100;
                         }
-                        translateX = (-50 - ((prevDivisor)*(prevCnt-i)));
-                        rotate = "rotate("+rotateVal+"deg)";
+                        translateX = (-50 - ((prevDivisor) * (prevCnt - i)));
+                        rotate = "rotate(" + rotateVal + "deg)";
 
-                        rotatePrevStart = rotatePrevStart + ((prevCnt*10) / prevCnt);
+                        rotatePrevStart = rotatePrevStart + ((prevCnt * 10) / prevCnt);
+
+                        console.log("index: " + index + "prevCnt: " + prevCnt + "nextCnt: " + nextCnt);
 
                         break;
                     default:
-                        translateX = (150 - ((prevDivisor*2)*i)) * -1;
+                        translateX = (150 - ((prevDivisor * 2) * i)) * -1;
                         rotate = "rotate(0deg)";
 
                 }
 
-                var styleStr = "translate("+ translateX +"%, 0%)  scale("+scale+") " + rotate;
-                
+                var styleStr = "translate(" + translateX + "%, 0%)  scale(" + scale + ") " + rotate;
+
                 z = z + 1;
 
                 els[i].style.transform = styleStr;
                 els[i].style.zIndex = z;
 
             }
-            
+
             // we are going for active element, so make it higher
             z = z - 1;
-            
+
             var j = 0;
 
             rotateNegStart = 0;
             scale = 1;
-            for(var i=prevCnt+1; i<nextCnt+prevCnt+1; i++) {
+            for (var i = prevCnt + 1; i < nextCnt + prevCnt + 1; i++) {
                 j = j + 1;
-                switch(layout) {
+                switch (layout) {
                     case "slide":
-                        scale = scale - (100 / (maxCntDivisor+1))/100;
-                        translateX = (50 - ((nextDivisor)*(j))) * -1;
+                        scale = scale - (100 / (maxCntDivisor + 1)) / 100;
+                        translateX = (50 - ((nextDivisor) * (j))) * -1;
                         translateY = vertOffsets[i] / 2;
 
                         rotate = "rotate(0deg)";
@@ -212,77 +211,77 @@
                     case "fanOut":
                         rotateVal = rotateNextStart;
 
-                        scale = scale - (100 / (maxCntDivisor+1))/100;
-                        translateX = (50 - ((nextDivisor)*(j))) * -1;
-                        rotate = "rotate("+rotateVal+"deg)";
+                        scale = scale - (100 / (maxCntDivisor + 1)) / 100;
+                        translateX = (50 - ((nextDivisor) * (j))) * -1;
+                        rotate = "rotate(" + 30 + "deg)";
 
-                        rotateNextStart = rotateNextStart + ((nextCnt*10) / nextCnt);
+                        rotateNextStart = rotateNextStart + ((nextCnt * 10) / nextCnt);
                         break;
                     default:
-                        translateX = (50 - ((prevDivisor*2)*i)) * -1;
+                        translateX = (50 - ((prevDivisor * 2) * i)) * -1;
                         rotate = "rotate(0deg)";
 
                 }
 
                 z = z - 1;
 
-                var styleStr = "translate("+ translateX +"%, 0%)  scale("+scale+") " + rotate;
+                var styleStr = "translate(" + translateX + "%, 0%)  scale(" + scale + ") " + rotate;
 
                 els[i].style.transform = styleStr;
                 els[i].style.zIndex = z;
             }
 
-    
+
 
         }
 
-        stackedCards.prototype.detectSwipe = function() {
+        stackedCards.prototype.detectSwipe = function () {
             var me = this;
             var regionEl = document.querySelector(me.config.selector);
 
-            me.detectSwipeDir(regionEl, function(swipedir){
+            me.detectSwipeDir(regionEl, function (swipedir) {
                 var activeEl = document.querySelector(me.config.selector + " li.active");
-                if (swipedir =='left') {
+                if (swipedir == 'left') {
                     activeEl.nextElementSibling.click();
                 }
-                else if(swipedir=="right") {
+                else if (swipedir == "right") {
                     activeEl.previousElementSibling.click();
                 }
             })
 
         }
 
-        stackedCards.prototype.extend = function(custom, defaults) {
+        stackedCards.prototype.extend = function (custom, defaults) {
             var key, value;
             for (key in defaults) {
                 value = defaults[key];
                 if (custom[key] == null) {
-                  custom[key] = value;
+                    custom[key] = value;
                 }
             }
             return custom;
         }
 
-        stackedCards.prototype.nodelistToArray = function(nodelist) {
+        stackedCards.prototype.nodelistToArray = function (nodelist) {
             var results = [];
             var i, element;
-            for(i=0; i < nodelist.length; i++) {
+            for (i = 0; i < nodelist.length; i++) {
                 element = nodelist[i];
                 results.push(element);
             }
             return results;
         }
 
-        stackedCards.prototype.loopNodeList = function(els, callback, scope) {
+        stackedCards.prototype.loopNodeList = function (els, callback, scope) {
             for (var i = 0; i < els.length; i++) {
                 callback.call(scope, els[i])
             }
         }
 
 
-        stackedCards.prototype.scrolledIn = function(el, offset) {
-            if(typeof el == 'undefined') return;
-  
+        stackedCards.prototype.scrolledIn = function (el, offset) {
+            if (typeof el == 'undefined') return;
+
             var elemTop = el.getBoundingClientRect().top;
             var elemBottom = el.getBoundingClientRect().bottom;
 
@@ -291,24 +290,24 @@
 
         }
 
-        stackedCards.prototype.detectSwipeDir = function(el, callback) {
-            
+        stackedCards.prototype.detectSwipeDir = function (el, callback) {
+
             //credits: http://www.javascriptkit.com/javatutors/touchevents2.shtml
 
             var touchsurface = el,
-            swipedir,
-            startX,
-            startY,
-            distX,
-            distY,
-            threshold = 75, //required min distance traveled to be considered swipe
-            restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-            allowedTime = 300, // maximum time allowed to travel that distance
-            elapsedTime,
-            startTime,
-            handleswipe = callback || function(swipedir){}
-          
-            touchsurface.addEventListener('touchstart', function(e){
+                swipedir,
+                startX,
+                startY,
+                distX,
+                distY,
+                threshold = 75, //required min distance traveled to be considered swipe
+                restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+                allowedTime = 300, // maximum time allowed to travel that distance
+                elapsedTime,
+                startTime,
+                handleswipe = callback || function (swipedir) { }
+
+            touchsurface.addEventListener('touchstart', function (e) {
                 var touchobj = e.changedTouches[0]
                 swipedir = 'none'
                 dist = 0
@@ -317,22 +316,22 @@
                 startTime = new Date().getTime() // record time when finger first makes contact with surface
                 e.preventDefault()
             }, false)
-          
-            touchsurface.addEventListener('touchmove', function(e){
-               // e.preventDefault() // prevent scrolling when inside DIV
+
+            touchsurface.addEventListener('touchmove', function (e) {
+                // e.preventDefault() // prevent scrolling when inside DIV
             }, false)
-          
-            touchsurface.addEventListener('touchend', function(e){
+
+            touchsurface.addEventListener('touchend', function (e) {
                 var touchobj = e.changedTouches[0]
                 distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
                 distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
                 elapsedTime = new Date().getTime() - startTime // get time elapsed
-                if (elapsedTime <= allowedTime){ // first condition for awipe met
-                    if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                        swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+                if (elapsedTime <= allowedTime) { // first condition for awipe met
+                    if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+                        swipedir = (distX < 0) ? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
                     }
-                    else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                        swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+                    else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+                        swipedir = (distY < 0) ? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
                     }
                 }
                 handleswipe(swipedir)
